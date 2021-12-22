fbeb1f9bea0d909a823a0f241d52adb42783dc97
rsd13 <rsoriadiez@gmail.com>
2021-12-22 18:07:36 +0100

    recogido API AEMET la información en texto

--- fuctions/api/aemet.py
+++ fuctions/api/aemet.py
@@ -3,6 +3,15 @@ import json
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+from datetime import date
+
+
+
+CODIGOS_PROVINCIA_AEMET={
+    'alicante':"03"
+}
+
+
 
 
 def getParameterEnv(name):
@@ -25,29 +34,29 @@ def getEnv():
     #toma las variables de entorno de .env.
     load_dotenv(file_env)
    
-def authRequest(date):
+def authRequest():
     """
     se encarga de recoger y guardar en la base de datos toda la información númerica y/o texto de los datos de AEMT
     dates<lst>: lista que contiene el rango de fechas para recuperar la información
     return<json>: JSON con la información obtenida en la API
     """
-
     params = {
         'api_key': getParameterEnv("KEY")
     }
-    url = getParameterEnv("URL_NUM") + date
-    print(url)
+    url = getParameterEnv("URL_TEXT_HOY").format(var1=CODIGOS_PROVINCIA_AEMET['alicante']) + date.today().strftime("%Y-%m-%d")
     headers = {
          'cache-control': "no-cache"
     }
 
     response = requests.get(url,headers=headers, params=params)
+    
     return response.json()
+    
 
 
 
 
-def run_api(arg,dates):
+def run_api(arg, **kwargs):
     """
     se encarga de recoger y guardar en la base de datos toda la información númerica y/o texto de los datos de AEMT
     arg<string>: argumento para ver que información recoger
@@ -56,7 +65,11 @@ def run_api(arg,dates):
     #consigue los .env
     getEnv()
     #hace llamada a la API
-   
+    if arg=="t":
+        json = authRequest()
+
+    """ 
     for date in range(int(dates[0]),int(dates[1])):
         json = authRequest(str(date))
         print(json)
+    """
--- fuctions/api/main.py
+++ fuctions/api/main.py
@@ -12,7 +12,7 @@ def parse():
     #grupo que limpia 
     grupo = parser.add_mutually_exclusive_group()      # grupo mutuamente excluyente (solo una operacion)
     #grupo.add_argument('-c', '--clear', help='Limpia el dataset y lo deja prepardo para el analisis',action='store_true')           # action guarda el argumento
-    grupo.add_argument('-t',action='store',nargs=2,dest='numero',default=[],help=HELP1)
+    grupo.add_argument('-t',dest='texto',action='store_true',default=[],help=HELP1)
     #grupo.add_argument('-d2',action='append', dest='d2',default=[],help="Añade los estados que se quiere comparar. Ej: -as California -as 'New York' ...")
 
     #parser.add_argument('string1', help='Primer numero de la operacion.',type=str)
@@ -23,12 +23,15 @@ def main():
     args=parse()
     # opciones
    
-    if args.numero:
+    if args.texto:
+        run_api(arg="t")
+        """
         try:
             [datetime.datetime.strptime(date, '%Y%m%d') for date in args.numero ]
         except ValueError:
             raise ValueError("Formato de fecha incorrecto, deberia ser YYYYMMDD")
-        run_api("n",args.numero)
+        """
+        
       
     else:
         print ('Error: se requiere uno o mas argumentos para realizar la accion. Pulsa -h para más información')
